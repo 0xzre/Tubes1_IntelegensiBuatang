e2f479b10a099cced40dbc728e6c003c16caf1c1
Agilham <13521118@std.stei.itb.ac.id>
2023-10-18 15:33:44 +0700

    fix: local bot

--- src/LocalBot.java
+++ src/LocalBot.java
@@ -1,5 +1,7 @@
 import javafx.scene.control.Button;
 
+import java.util.*;
+
 public class LocalBot extends Bot {
     @Override
     public int[] move(int roundsLeft, boolean isBotFirst, int playerOScore, int playerXScore, Button[][] buttons,
@@ -14,9 +16,7 @@ public class LocalBot extends Bot {
             }
         }
 
-        int[] allValues;
-        int[] rowValues;
-        int[] colValues;
+        List<List<Integer>> objectiveValues = new ArrayList<>();
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
                 if (curButtons[i][j].getText().equals("")) {
@@ -25,19 +25,61 @@ public class LocalBot extends Bot {
                     } else {
                         curButtons[i][j].setText("O");
                     }
+                    int difference = updateGameBoard(true, i, j, curButtons);
+                    int curValue;
+                    if (isMaximizingX) {
+                        curPlayerXScore += (difference + 1);
+                        curValue = curPlayerXScore-playerXScore;
+                    } else {
+                        curPlayerOScore += (difference + 1);
+                        curValue = curPlayerOScore-playerOScore;
+                    }
+                    objectiveValues.add(List.of(curValue, i, j));
                 }
-                int difference = updateGameBoard(true, i, j, curButtons);
-                if (isMaximizingX) {
-                    curPlayerXScore += (difference + 1);
-                    curPlayerOScore -= difference;
-                } else {
-                    curPlayerOScore += (difference + 1);
-                    curPlayerXScore -= difference;
-                }
+
+                restoreButtons(curButtons, buttons);
+                curPlayerOScore = playerOScore;
+                curPlayerXScore = playerXScore;
             }
         }
-        int[] bestValues = new int[3];
 
+        filterObjectiveValues(objectiveValues);
+        return getRandomBestValues(objectiveValues);
+    }
+
+    public void restoreButtons(Button[][] curButtons, Button[][] backUpButtons) {
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                curButtons[i][j] = new Button(backUpButtons[i][j].getText()); // Create a new Button with the same properties
+            }
+        }
+    }
+
+    public void printValues(ArrayList<Integer> array) {
+        System.out.println("Values: ");
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+    }
+
+    public void filterObjectiveValues(List<List<Integer>> objectiveValues) {
+        int maxLeftValue = objectiveValues.stream()
+                .max(Comparator.comparing(triplet -> triplet.get(0)))
+                .map(triplet -> triplet.get(0))
+                .orElse(Integer.MIN_VALUE);
+        objectiveValues.removeIf(triplet -> triplet.get(0) != maxLeftValue);
+    }
+
+    public int[] getRandomBestValues(List<List<Integer>> objectiveValues) {
+        Collections.shuffle(objectiveValues, new Random());
+        int[] bestValues = new int[3];
+        if (!objectiveValues.isEmpty()) {
+            List<Integer> randomTriplet = objectiveValues.get(0);
+            for (int i = 0; i < 3 && i < randomTriplet.size(); i++) {
+                bestValues[i] = randomTriplet.get(i);
+            }
+        }
+        // Return a subset of bestValues
         return new int[]{bestValues[1], bestValues[2]};
     }
 }
